1212c24543bd2da493460eafed576b3d10875805
Anselmo <falc@null.net>
2015-10-06 11:10:16 +0200

    edit box properties added

--- src/util.js
+++ src/util.js
@@ -294,7 +294,10 @@ var menuLayer = cc.Layer.extend({
     this.addChild(spritebg);
     
     np = new cc.EditBox(cc.size(5,10), cc.Scale9Sprite.create(res.greenbox_png));
+    np.setText("EditBox Sample");
     np.setPosition(centerpos);
+    np.setFontColor(cc.c3b(255, 250, 0));
+    np.setDelegate(this);
     this.addChild(np);
   },
   onPlay: function()
