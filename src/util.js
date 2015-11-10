08ff9f2b9d8df60aeec2041f70404c40dc8eccf7
Anselmo <falc@null.net>
2015-11-10 11:00:59 +0100

    PopUp Menu

--- src/util.js
+++ src/util.js
@@ -237,6 +237,7 @@ var animLayer = cc.Layer.extend({
             parent.menu.addItem(ab2);
             parent.menu.addItem(ab3);
             parent.menu.addItem(ab4);
+            parent.menu.setPosition(0,0);
             var mcoord = parent.menu.getPosition();
             cc.log("menu: "+mcoord.x+", "+mcoord.y);
             //parent.menu.setPosition(ox-mcoord.x,oy-mcoord.y);
