b483fa3044277cc251bbdce8b206d530ec5a9c88
arkaik <falc@null.net>
2015-11-20 21:11:41 +0100

    Menu & things

--- src/util.js
+++ src/util.js
@@ -228,16 +228,15 @@ var animLayer = cc.Layer.extend({
           
           if (ok && plz && target.state == "alone")
           {
-            cc.log(parent.getName());
             event.stopPropagation();
             //TO DO: Subclass of (Menu) and (MenuItem): CircularMenu, CircularMenuItem.
             var cmenu = new CircularMenu(4,32);
             cmenu.setName("CName");
             cc.log("I'm creating the menu");
-            var ab1 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.buttons[0]),new cc.Sprite(res.button_png, hw.buttons[0]), function(){cc.log("ab1"); cc.log(this.getName());}, parent); //this.removeChildbyTag(100);
-            var ab2 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.buttons[0]),new cc.Sprite(res.button_png, hw.buttons[0]), function(){cc.log("ab2"); cc.log(this.getName());}, parent);
-            var ab3 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.buttons[0]),new cc.Sprite(res.button_png, hw.buttons[0]), function(){cc.log("ab3");}, parent);
-            var ab4 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.buttons[0]),new cc.Sprite(res.button_png, hw.buttons[0]), function(){cc.log("ab4");}, parent);
+            var ab1 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.buttons[0]),new cc.Sprite(res.button_png, hw.buttons[0]), function(){cc.log("ab1"); cc.log(this.getName()); this.removeChildbyTag(100);}, parent); //this.removeChildbyTag(100);
+            var ab2 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.buttons[0]),new cc.Sprite(res.button_png, hw.buttons[0]), function(){cc.log("ab2"); cc.log(this.getName()); this.removeChildbyTag(100);}, parent);
+            var ab3 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.buttons[0]),new cc.Sprite(res.button_png, hw.buttons[0]), function(){cc.log("ab3"); this.removeChildbyTag(100);}, parent);
+            var ab4 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.buttons[0]),new cc.Sprite(res.button_png, hw.buttons[0]), function(){cc.log("ab4"); this.removeChildbyTag(100);}, parent);
             cmenu.addItem(ab1);
             cmenu.addItem(ab2);
             cmenu.addItem(ab3);
