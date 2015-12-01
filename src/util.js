a97e1a8969fa2127b30f43570d7c2f59264684e4
arkaik <falc@null.net>
2015-12-01 13:14:03 +0100

    Actions

--- src/util.js
+++ src/util.js
@@ -284,7 +284,7 @@ var animLayer = cc.Layer.extend({
             var ab4 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.actions[3]),new cc.Sprite(res.button_png, hw.actions[3]), f4(cmenu, target), parent);
             cmenu.addItems(ab1, ab2, ab3, ab4);
             cmenu.setPosition(ox*32,oy*32);
-            cmenu.setColor(cc.color(126,126,126));
+            //cmenu.setColor(cc.color(126,126,126));
             parent.addChild(cmenu,10,100);
           }
           else if (target.state == "selected")
@@ -321,6 +321,15 @@ var animLayer = cc.Layer.extend({
 
               target.state = "moving";
             }
+
+            var cmenu = new CircularMenu(3,48);
+            //var ab1 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.actions[0]),new cc.Sprite(res.button_png, hw.actions[0]), f1(cmenu, target), parent); 
+            var ab2 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.actions[1]),new cc.Sprite(res.button_png, hw.actions[1]), f2(cmenu, target), parent);
+            var ab3 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.actions[2]),new cc.Sprite(res.button_png, hw.actions[2]), function(){cc.log("ab3"); }, parent);
+            var ab4 = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.actions[3]),new cc.Sprite(res.button_png, hw.actions[3]), f4(cmenu, target), parent);
+            cmenu.addItems( ab2, ab3, ab4);
+            cmenu.setPosition(ox*32,oy*32);
+            parent.addChild(cmenu,10,100);
             
           }
           else if (target.state == "moving")
