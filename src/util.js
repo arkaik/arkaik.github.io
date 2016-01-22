ab6e07954ae6d6cd6116fc6b491969736d91e0e4
arkaik <falc@null.net>
2016-01-22 13:39:47 +0100

    Selection Menu

--- src/util.js
+++ src/util.js
@@ -112,7 +112,7 @@ var SelectionMenu = cc.Menu.extend({
       var bm = new cc.MenuItemSprite(new cc.Sprite(res.button_png, hw.buttons[k]), new cc.Sprite(res.button_png, hw.buttons[k]), function(){}, this);
       bm.setPosition(32*k,0);
       
-      cc.Menu.prototype.addChild.call(this,bm,zOrder,tag);
+      this.addChild(bm);
     }
 
     for (i = 0; i < hw.symbol.length; i++)
@@ -122,7 +122,7 @@ var SelectionMenu = cc.Menu.extend({
       var menui = new cc.MenuItemSprite(new cc.Sprite(res.image_png, symspr), new cc.Sprite(res.image_png, symspr), new cc.Sprite(res.image_png, symdis), function(){}, this);
       //mic[i] = menui;
       menui.setPosition(32*i,64);
-      cc.Menu.prototype.addChild.call(this,menui,zOrder,tag);
+      this.addChild(menui);
     }
 
     this._super();
